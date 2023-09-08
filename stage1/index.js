bc84a3a06385812c6fb67bda47274b104837b791
chigozieokoroafor <okoroaforc14@gmail.com>
2023-09-08 14:42:53 +0100

    done with stage 1 of HNG task

---
+++ stage1/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/HNG/api", (req, res)=>{
+    const {slack_name, track} = req.query;
+    const date = new Date();
+
+    const data = {
+        slack_name:slack_name,
+        current_day: date.getFullYear(),
+        utc_time: date,
+        track:track,
+        github_file_url: "https://github.com/chigozieokoroafor/HNGs1/blob/main/stage1/index.js",
+        github_repo_url: "https://github.com/chigozieokoroafor/HNGs1",
+        status_code:200
+    }
+    return res.send(data).status(200);
+});
+
+app.listen(10000, ()=>{
+
+})
